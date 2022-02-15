4c46aa70b64f5743e24575c5b9382fd3edb3e323
Mikael Turøy <mikael.turoy@holbergeeg.org>
2022-02-15 13:51:39 +0100

    Add project files.

---
+++ Lost Ark Launcher/LostArkSettings.cs
@@ -0,0 +1,53 @@
+﻿using System.Xml.Linq;
+using System.Xml.XPath;
+
+namespace Lost_Ark_Launcher
+{
+    internal class LostArkSettings
+    {
+        public LostArkSettings(string path)
+        {
+            FilePath = path;
+        }
+
+        public string FilePath { get; }
+
+        public string? GetXmlPathValue(string path)
+        {
+            return GetXmlPath(path)?.Value;
+        }
+
+        public XElement? GetXmlPath(string path)
+        {
+            var config = OpenConfigFile();
+            if (config != null)
+            {
+                var region = config.XPathSelectElement(path);
+                if (region != null)
+                {
+                    return region;
+                }
+            }
+            return null;
+        }
+
+        public void SetXmlPath(string path, string value)
+        {
+            var val = GetXmlPath(path);
+            if (val != null)
+                val.Value = value;
+            Save(val.Document);
+        }
+
+        private XDocument OpenConfigFile()
+        {
+            return XDocument.Load(FilePath);
+        }
+
+        public void Save(XDocument xml)
+        {
+            xml.Save(FilePath);
+        }
+
+    }
+}
---
+++ Lost Ark Launcher/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Lost_Ark_Launcher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string BasePath;
+        const string ConfigFile = @"\EFGame\Config\UserOption.xml";
+        const string ExeFile = @"Binaries\Win64\Launch_Game.exe";
+        const string XmlRegionPath = @"/UserOption/SaveAccountOptionData/RegionID";
+        const int SteamGameId = 1599340;
+
+        public static Dictionary<string, string> Regions = new Dictionary<string, string> {
+            {"Europe", "CE"},
+            {"North America East", "EA" },
+            {"North America West", "WA" },
+            {"South America", "SA" },
+        };
+
+        internal LostArkSettings Config { get; private set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupFilePaths();
+            SetRegionChoices();
+        }
+
+        private void SetupFilePaths()
+        {
+            using (RegistryKey key = Registry.LocalMachine.OpenSubKey($@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\Steam App {SteamGameId}"))
+            {
+                if (key != null)
+                {
+                    BasePath = (string)key.GetValue("InstallLocation");
+                    if (BasePath != null)
+                    {
+                        Config = new LostArkSettings(System.IO.Path.Combine(BasePath + ConfigFile));
+                    }
+                }
+            }
+        }
+
+        private void SetRegionChoices()
+        {
+            if (Config == null) return;
+
+            var activeRegion = Config.GetXmlPathValue(XmlRegionPath);
+            RegionChoice.Items.Clear();
+            foreach (var entry in Regions)
+            {
+                RegionChoice.Items.Add(entry.Key);
+                if (activeRegion == entry.Value)
+                {
+                    RegionChoice.SelectedItem = entry.Key;
+                }
+            }
+        }
+
+        private void RegionChoice_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var key = RegionChoice.SelectedItem as string;
+            if (key != null && Regions.ContainsKey(key)) {
+                var val = Regions[key];
+                if (val != null)
+                    Config.SetXmlPath(XmlRegionPath, val);
+            }
+        }
+
+        private void LaunchGameButton_Click(object sender, RoutedEventArgs e)
+        {
+            var psinfo = new ProcessStartInfo();
+            psinfo.UseShellExecute = true;
+            psinfo.FileName = $"steam://rungameid/{SteamGameId}";
+            var p =  Process.Start(psinfo);
+            p.Exited += P_Exited;
+            LaunchGameButton.IsEnabled = false;
+            LaunchGameButton.Content = "Game started";
+        }
+
+        private void P_Exited(object? sender, EventArgs e)
+        {
+            LaunchGameButton.Content = "Launch game";
+            LaunchGameButton.IsEnabled=true;
+        }
+    }
+}
